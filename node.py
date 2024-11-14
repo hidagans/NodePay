69fb9ccf8bcc21b3509e24d6783d77bcc9f260f5
IDWR <35910302+IDWR2016@users.noreply.github.com>
2024-11-14 17:44:41 +0700

    Update node.py

--- node.py
+++ node.py
@@ -35,7 +35,7 @@ TOKEN_FILE = 'np_tokens_1.txt'
 
 DOMAIN_API = {
     "SESSION": "https://api.nodepay.ai/api/auth/session",
-    "PING": "http://13.215.134.222/api/network/ping"
+    "PING": "http://52.77.10.116/api/network/ping"
 }
 
 CONNECTION_STATES = {
