068c75ce754ae1f25010163da51d26dc4dccfc95
hidagans <58388773+hidagans@users.noreply.github.com>
2024-11-18 21:57:32 +0700

    Update node.py

--- node.py
+++ node.py
@@ -35,7 +35,7 @@ TOKEN_FILE = 'np_tokens_1.txt'
 
 DOMAIN_API = {
     "SESSION": "https://api.nodepay.ai/api/auth/session",
-    "PING": "http://13.215.134.222/api/network/ping"
+    "PING": "https://nw.nodepay.org/api/network/ping"
 }
 
 CONNECTION_STATES = {
